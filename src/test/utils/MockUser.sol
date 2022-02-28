81477746aa4b78dad99de4a1a979bcb8c1056bfc
alfred <fuzzy@lemmasis.org>
2022-02-28 12:27:33 -0500

    move interface to directory

--- src/test/utils/MockUser.sol
+++ src/test/utils/MockUser.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.10;
 
-import {IFaucet} from "../../IFaucet.sol";
+import {IFaucet} from "../../interfaces/IFaucet.sol";
 
 contract MockUser {
 
