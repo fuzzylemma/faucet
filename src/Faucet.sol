a2285529e554342ba4bcb6edadc1bc58fe98ca93
alfred <fuzzy@lemmasis.org>
2022-02-28 20:09:08 -0500

    remove getters, add basic tests

--- src/Faucet.sol
+++ src/Faucet.sol
@@ -10,14 +10,14 @@ contract Faucet {
     using SafeERC20 for IERC20;
 
     // token => faucet owner
-    mapping(address => address) internal plumber;
+    mapping(address => address) public plumber;
     // token => drip size 
-    mapping(address => uint256) internal dripSize;
+    mapping(address => uint256) public dripSize;
     // token => time between faucet requests;
     //      a value of 0 implies faucet is closed 
-    mapping(address => uint256) internal dripFrequency;
+    mapping(address => uint256) public dripFrequency;
     // token => account => last request
-    mapping(address => mapping(address => uint256)) internal lastSip;
+    mapping(address => mapping(address => uint256)) public lastSip;
 
     modifier onlyPlumber(address token) {
         require(plumber[token] == msg.sender, "must be plumber");
@@ -35,7 +35,6 @@ contract Faucet {
 
         // send faucet balance to plumber
         uint256 balance = IERC20(token).balanceOf(address(this));
-        IERC20(token).approve(token, balance);
         IERC20(token).safeTransfer(plumber[token], balance);
 
         delete plumber[token];
@@ -45,32 +44,27 @@ contract Faucet {
 
     }
 
-    function getDrip(address token) external returns (uint256) {
-        return dripSize[token];
-    }
-
-    function setDrip(address token, uint256 drip) external onlyPlumber(token) {
-        dripSize[token] = drip;
-    }
-
-    function getDripFrequency(address token) external returns (uint256) {
-        return dripFrequency[token];
-    }
-
-    function setDripFrequency(address token, uint256 frequency) external onlyPlumber(token) {
-        dripFrequency[token] = frequency;
-    }
-
     function donateToFaucet(address token, uint256 amount) external {
         IERC20(token).safeTransferFrom(msg.sender, address(this), amount);
     }
 
     function request(address token) external /*nonEntrant*/ {
         require(dripFrequency[token] > 0, "faucet closed");
-        require(block.timestamp > lastSip[token][msg.sender] + dripFrequency[token], "request too high frequency");
+        if (lastSip[token][msg.sender] != 0) {
+            require(block.timestamp > lastSip[token][msg.sender] + dripFrequency[token], "request too high frequency");
+        }
         require(IERC20(token).balanceOf(address(this)) >= dripSize[token], "faucet dry");
+
         lastSip[token][msg.sender] = block.timestamp;
         IERC20(token).safeTransfer(msg.sender, dripSize[token]);
     }
-    
+ 
+    function setDrip(address token, uint256 drip) external onlyPlumber(token) {
+        dripSize[token] = drip;
+    }
+
+    function setDripFrequency(address token, uint256 frequency) external onlyPlumber(token) {
+        dripFrequency[token] = frequency;
+    }
+   
 }
--- src/test/Faucet.t.sol
+++ src/test/Faucet.t.sol
@@ -6,10 +6,17 @@ import {ERC20Mock} from "solidstate-solidity/token/ERC20/ERC20Mock.sol";
 import {Faucet} from "../Faucet.sol";
 import {MockUser} from "./utils/MockUser.sol";
 
+interface CheatCodes {
+    function warp(uint) external;
+}
+
 contract FaucetTest is DSTest {
+
+    CheatCodes cheats = CheatCodes(HEVM_ADDRESS);
 
     ERC20Mock mockA;
     ERC20Mock mockB;
+    MockUser alice;
 
     Faucet faucet;
 
@@ -20,19 +27,132 @@ contract FaucetTest is DSTest {
 
     function testCreateFaucet() public {
         faucet = new Faucet();
+
+        uint dripSize = 1000 ether;
+        uint dripFrequency = 60 * 60 * 24 * 7; 
+
+        faucet.createFaucet(address(mockA), dripSize, dripFrequency);
+
+        assertEq(faucet.dripFrequency(address(mockA)), dripFrequency);
+        assertEq(faucet.dripSize(address(mockA)), dripSize);
+        assertEq(faucet.plumber(address(mockA)), address(this));
     }
-    function testDonateToFaucet() public {}
-    function testRequest() public {}
-    function testDestroyFaucet() public {} 
 
-    /*
-    function destroyFaucet(address token) external; 
-    function getDrip(address token) external returns (uint256); 
-    function setDrip(address token, uint256 drip) external; 
-    function getDripFrequency(address token) external returns (uint256); 
-    function setDripFrequency(address token, uint256 frequency) external; 
-    function donateToFaucet(address token, uint256 amount) external; 
-    function request(address token) external ; 
-    */
+    function testDonateToFaucet() public {
+        faucet = new Faucet();
 
+        uint supply = 1000000 ether;
+
+        mockA.__mint(address(this), supply);
+        mockA.approve(address(faucet), supply);
+
+        uint dripSize = 1000 ether;
+        uint dripFrequency = 60 * 60 * 24 * 7; 
+
+        faucet.createFaucet(address(mockA), dripSize, dripFrequency);
+        faucet.donateToFaucet(address(mockA), supply);
+
+        assertEq(mockA.balanceOf(address(faucet)), supply);
+    }
+
+    function testRequest() public {
+        faucet = new Faucet();
+        alice = new MockUser(address(faucet));
+
+        uint supply = 1000000 ether;
+        mockA.__mint(address(this), supply);
+        mockA.approve(address(faucet), supply);
+
+        uint dripSize = 1000 ether;
+        uint dripFrequency = 60 * 60 * 24 * 7; 
+
+        faucet.createFaucet(address(mockA), dripSize, dripFrequency);
+        faucet.donateToFaucet(address(mockA), supply);
+        
+        alice.request(address(mockA));
+        assertEq(mockA.balanceOf(address(alice)), dripSize);
+    }
+
+    function testFailDestroyFaucetNotPlumber() public {
+        faucet = new Faucet();
+        alice = new MockUser(address(faucet));
+        faucet.createFaucet(address(mockA), 0, 0);
+        alice.destroyFaucet(address(mockA));
+    }
+
+    function testDestroyFaucet() public {
+        faucet = new Faucet();
+        alice = new MockUser(address(faucet));
+
+        uint supply = 1000000 ether;
+        mockA.__mint(address(this), supply);
+        mockA.approve(address(faucet), supply);
+
+        uint dripSize = 1000 ether;
+        uint dripFrequency = 60 * 60 * 24 * 7; 
+
+        faucet.createFaucet(address(mockA), dripSize, dripFrequency);
+        faucet.destroyFaucet(address(mockA));
+
+        assertEq(faucet.dripFrequency(address(mockA)), 0);
+        assertEq(faucet.dripSize(address(mockA)), 0);
+        assertEq(faucet.plumber(address(mockA)), address(0));
+
+        faucet.createFaucet(address(mockA), dripSize, dripFrequency);
+        faucet.donateToFaucet(address(mockA), supply);
+        alice.request(address(mockA));
+        cheats.warp(dripFrequency);
+        alice.request(address(mockA));
+        faucet.destroyFaucet(address(mockA));
+
+        assertEq(mockA.balanceOf(address(this)), supply-(2*dripSize));
+    }
+
+    function testFailClosedFaucet() public {
+        faucet = new Faucet();
+        alice = new MockUser(address(faucet));
+
+        uint supply = 1000000 ether;
+        mockA.__mint(address(this), supply);
+        mockA.approve(address(faucet), supply);
+
+        uint dripSize = 1000 ether;
+        uint closed = 0; 
+
+        faucet.createFaucet(address(mockA), dripSize, closed);
+        faucet.donateToFaucet(address(mockA), supply);
+        alice.request(address(mockA));
+    }
+
+    function testSetDripSize() public {
+        faucet = new Faucet();
+        uint dripSize = 1000 ether;
+        faucet.createFaucet(address(mockA), dripSize, 0);
+        faucet.setDrip(address(mockA), 0);
+        assertEq(faucet.dripSize(address(mockA)), 0);
+    }
+
+    function testFailSetDripSizeNotPlumber() public {
+        faucet = new Faucet();
+        alice = new MockUser(address(faucet));
+        uint dripSize = 1000 ether;
+        faucet.createFaucet(address(mockA), dripSize, 0);
+        alice.setDrip(address(mockA), 0);
+    }
+
+    function testSetDripFrequency() public {
+        faucet = new Faucet();
+        uint dripFrequency = 60 * 60 * 24 * 7;
+        faucet.createFaucet(address(mockA), 0, dripFrequency);
+        faucet.setDripFrequency(address(mockA), 0);
+        assertEq(faucet.dripFrequency(address(mockA)), 0);
+    }
+
+    function testFailSetDripFrequencyNotPlumber() public {
+        faucet = new Faucet();
+        alice = new MockUser(address(faucet));
+        uint dripFrequency = 60 * 60 * 24 * 7;
+        faucet.createFaucet(address(mockA), 0, dripFrequency);
+        alice.setDripFrequency(address(mockA), 0);
+    }
 }
