f75dfa3708d5e7733bbd44c56c4deb8210335798
alfred <fuzzy@lemmasis.org>
2022-03-07 09:23:58 -0500

    renounce faucet for owner

--- src/FaucetFactory.sol
+++ src/FaucetFactory.sol
@@ -5,19 +5,30 @@ import {Faucet} from "./Faucet.sol";
 
 contract FaucetFactory {
 
-    Faucet[] faucets;
+    address[] public faucets;
+    mapping(address => address) ownerToFaucet;
 
     event FaucetCreated(address faucetAddress, address owner);
+    event FaucetAbandoned(address faucetAddress, address owner);
 
     function allFaucetsLength() external view returns (uint) {
         return faucets.length;
     }
 
     function create_faucet() external {
+        require(ownerToFaucet[msg.sender] == address(0), "account has a faucet");
         Faucet faucet = new Faucet();
-        faucets.push(faucet);
         faucet.transferOwnership(msg.sender);
+        faucets.push(address(faucet));
+        ownerToFaucet[msg.sender] = address(faucet);
         emit FaucetCreated(address(faucet), msg.sender);
+    }
+
+    function abandon_faucet(address faucet) external {
+        require(ownerToFaucet[msg.sender] == faucet, "not faucet owner");
+        ownerToFaucet[msg.sender] = address(0);
+        delete ownerToFaucet[msg.sender];
+        emit FaucetAbandoned(faucet, msg.sender);
     }
 
 }
